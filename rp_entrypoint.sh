9d97f40e66fd977482dfae70889e75186edf5741
Ni Tianzhen <nitianzhen1210@hotmail.com>
2025-07-04 02:01:48 +0800

    chore: change torch cache dir

--- rp_entrypoint.sh
+++ rp_entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /runpod-volume/Stable3DGen || exit 1
 git pull
-exec /runpod-volume/Stable3DGen/venv/bin/python -u rp_handler.py
+TORCH_HOME=/runpod-volume/Stable3DGen/.cache/torch exec /runpod-volume/Stable3DGen/venv/bin/python -u rp_handler.py
